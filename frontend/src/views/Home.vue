c686e1f3cba9cd0575e5daf004b5a984be431540
AlexBOYERE <proalexandreboyere@gmail.com>
2025-09-20 20:20:51 +0200

    init

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="p-6">
+    <h1 class="text-3xl font-bold">Bienvenue sur Tulipe 🌷</h1>
+    <p class="mt-4 text-gray-600">Ceci est la page d’accueil.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
